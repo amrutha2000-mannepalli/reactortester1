03367b9a23e209d0c31c83a3c366a8d9f6c0b715
amrutha2000-mannepalli <ammumannepalli2000@gmail.com>
2025-07-07 10:55:13 +0530

    Updation after review

--- reactiontester.js
+++ reactiontester.js
@@ -12,10 +12,10 @@ var clicktime= new Date().getTime();
 var left;
 var top;
 var heightwidth;
-left=Math.random() *150;//random number between 1 to 150
+left=Math.random() *300;//random number between 1 to 150
 console.log(left)
-top=Math.random() *150;
-heightwidth=((Math.random() *300) +100);// random number between 100 to 300  where +100 means starting from min heightwidth 100
+top=Math.random() *300;
+heightwidth=((Math.random() *500) +100);// random number between 100 to 300  where +100 means starting from min heightwidth 100
             document.getElementById("shape").style.left=left+ "px";
             document.getElementById("shape").style.top=top+ "px";
             document.getElementById("shape").style.height=heightwidth+ "px";
@@ -33,6 +33,6 @@ heightwidth=((Math.random() *300) +100);// random number between 100 to 300  whe
             console.log(clickendtime);
             var timetaken=(clickendtime-clicktime)/1000;
             console.log(timetaken)
-            alert(timetaken);
+            alert("time taken:"+timetaken+"seconds");
             Move();
         }
